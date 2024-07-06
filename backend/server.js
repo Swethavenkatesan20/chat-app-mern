39ed9364071ab80dd590cdd7d6d56e9d66d5315e
Swethavenkatesan20 <swethavenkatesan20@gmail.com>
2024-07-06 17:19:44 +0530

    updated for deployment changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-
+ 
 const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
